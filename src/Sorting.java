7baf146609085393812d084312321f2eeff7a6a8
Logan Schmillen <schmillen20@gmail.com>
2024-08-26 17:07:15 -0500

    added a sorting class file to have different sorting algorithms as method. started with bubble sort

---
+++ src/Sorting.java
@@ -0,0 +1,20 @@
+public class Sorting {
+    
+    public Sorting() {
+
+    }
+
+    public void bubbleSort(int[] array) {
+        for(int i = 0; i < array.length; i++) {
+            for(int j = 1; j < array.length - i; j++) {
+                if(array[j] < array[j - 1]) {
+                    int temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                }
+            }
+        }
+    }
+
+    
+}
