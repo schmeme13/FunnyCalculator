6ddfb4b9dd4112a6826989dc8d70ad5bc3407841
Logan Schmillen <schmillen20@gmail.com>
2024-08-26 16:39:54 -0500

    updated main to fit stack needs

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,12 @@
 public class Main {
     public static void main(String args[]) {
-        LinkedList list = new LinkedList();
+        Stack stack = new Stack();
 
-        list.add(1);
-        list.add(2);
-        list.add(3);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
 
-        list.remove(2);
-        list.add(3);
-
-        list.print();
-
-        System.out.println("Value count for no. 3: " + list.valueCount(3));
+        stack.print();
     }
 
 }
