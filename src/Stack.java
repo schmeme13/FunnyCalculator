76035544320cedf35d8a4bef413a4b2fb0fbbf05
Logan Schmillen <schmillen20@gmail.com>
2024-08-26 16:37:29 -0500

    added push method

--- src/Stack.java
+++ src/Stack.java
@@ -1,3 +1,13 @@
 public class Stack {
-    
+    Node top;
+
+    public Stack() {
+        this.top = null;
+    }
+
+    public void push(int data) {
+        Node node = new Node(data);
+        node.next = top;
+        top = node;
+    }
 }
