4fe736f12518144f3d1d59c68ece7d23dd36242c
Logan Schmillen <schmillen20@gmail.com>
2024-08-27 14:26:11 -0500

    added insertion sort

--- src/Sorting.java
+++ src/Sorting.java
@@ -28,6 +28,20 @@ public class Sorting {
         }
     }
 
+    public void insertionSort(int[] array) {
+        for(int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+
+            while(j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+
+            array[j + 1] = key;
+        }
+    }
+
     public void print(int[] array) {
         for(int i = 0; i < array.length - 1; i++) {
             System.out.print(array[i] + ", ");
