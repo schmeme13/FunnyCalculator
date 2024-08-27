6435f5d8510d22f7e662f5a482b62ca926636fb8
Logan Schmillen <schmillen20@gmail.com>
2024-08-27 15:08:42 -0500

    added quick sort

--- src/Sorting.java
+++ src/Sorting.java
@@ -48,7 +48,37 @@ public class Sorting {
 
     //Worse: O(n^2), Average: O(nlogn), Best: O(nlogn), Space: O(logn) or O(n) if pivot creates unbalanced partitions
     public void quickSort(int[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
 
+    private void quickSort(int[] array, int low, int high) {
+        if(low < high) {
+            int pivotIndex = partition(array, low, high);
+
+            quickSort(array, low, pivotIndex - 1);
+            quickSort(array, pivotIndex + 1, high);
+        }
+    }
+
+    private int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+
+        int i = low - 1;
+
+        for(int j = low; j < high; j++) {
+            if(array[j] < pivot) {
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+
+        return i + 1;
     }
 
     public void print(int[] array) {
